6affd96721d26f4a0817a2d5a0b8a082d7a417ac
Aditya Gupta <guptaaditya0530@gmail.com>
2026-06-02 18:11:46 +0530

    React router

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+
+
+    </>
+  )
+}
+
+export default App
